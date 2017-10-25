82cfbda1271d6cbb398e357ea6499eef95e78db7
Airat08 <myhytdinov@yandex.ru>
2017-10-25 20:59:46 +0400

    Laba 3 First pull request

--- src/PO52/Myhytdinov/wdad/data/managers/PreferencesManager.java
+++ src/PO52/Myhytdinov/wdad/data/managers/PreferencesManager.java
@@ -1,5 +1,6 @@
 package PO52.Myhytdinov.wdad.data.managers;
 
+import PO52.Myhytdinov.wdad.PreferencesConstantManager;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -13,11 +14,13 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.util.Properties;
+import java.util.Enumeration;
 
 public class PreferencesManager {
     private static PreferencesManager instance;
     private Document doc;
-    private static final String FILE_PATH="src\\PO52\\Myhytdinov\\wdad\\resources\\configuration\\appconfig.xml";
+    private static final String FILE_PATH = "src\\PO52\\Myhytdinov\\wdad\\resources\\configuration\\appconfig.xml";
 
     private PreferencesManager() throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -31,63 +34,73 @@ public class PreferencesManager {
             instance = new PreferencesManager();
         return instance;
     }
-
+    @Deprecated
     private Element getElement(String nameField) {
         NodeList nodeList = doc.getElementsByTagName(nameField);
         Element element = (Element) nodeList.item(0);
         return element;
     }
-
+    @Deprecated
     public String getCreateregistry() {
         return getElement("createregistry").getTextContent();
     }
 
+    @Deprecated
     public void setCreateregistry(String value) {
         getElement("createregistry").setTextContent(value);
     }
 
+    @Deprecated
     public String getRegistryaddress() {
         return getElement("registryaddress").getTextContent();
     }
 
+    @Deprecated
     public void setRegistryaddress(String value) {
         getElement("registryaddress").setTextContent(value);
     }
 
+    @Deprecated
     public int getRegistryport() {
         return Integer.parseInt(getElement("registryport").getTextContent());
     }
 
+    @Deprecated
     public void setRegistryport(int value) {
         getElement("registryport").setTextContent(String.valueOf(value));
     }
 
+    @Deprecated
     public String getPolicypath() {
         return getElement("policypath").getTextContent();
     }
 
+    @Deprecated
     public void setPolicypath(String value) {
         getElement("policypath").setTextContent(value);
     }
 
+    @Deprecated
     public String getUsecodebaseonly() {
         return getElement("usecodebaseonly").getTextContent();
     }
 
+    @Deprecated
     public void setUsecodebaseonly(String value) {
         getElement("usecodebaseonly").setTextContent(value);
     }
 
+    @Deprecated
     public String getClassprovider() {
         return getElement("classprovider").getTextContent();
     }
 
+    @Deprecated
     public void setClassprovider(String value) {
         getElement("classprovider").setTextContent(value);
     }
 
-    public void saveTransformXml()
-    {
+    public void saveTransformXml() {
         try {
             Transformer transformer = TransformerFactory.newInstance()
                     .newTransformer();
@@ -98,6 +111,58 @@ public class PreferencesManager {
             System.out.println(ex.getMessage());
         } catch (TransformerException ex) {
             System.out.println(ex.getMessage());
+        }
+    }
+
+    public void setProperty(String key, String value) {
+        getElement(key).setTextContent(value);
+    }
+
+    public String getProperty(String key) {
+        return getElement(key).getTextContent();
+    }
+
+    public void setProperties(Properties prop) {
+        while (prop.elements().hasMoreElements()) {
+            getElement((String) prop.elements().nextElement()).setTextContent((String) prop.keys().nextElement());
+        }
+    }
+
+    public Properties getProperties()
+    {
+        Properties prop = new Properties();
+        String[] keys = new String[]{
+                PreferencesConstantManager.CREATEREGISTRY,
+                PreferencesConstantManager.REGISTRYADDRESS,
+                PreferencesConstantManager.REGISTRYPORT,
+                PreferencesConstantManager.POLICYPATH,
+                PreferencesConstantManager.USECODEBASEONLY,
+                PreferencesConstantManager.CLASSPROVIDER
+        };
+
+        for (String key : keys) {
+            setProperty(key, getElement(key).getTextContent());
+        }
+
+        return prop;
+    }
+    public void addBindedObject(String name, String className)
+    {
+        Element element=(Element) doc.createElement("bindedobject");
+        element.setAttribute("class",className);
+        element.setAttribute("name",name);
+        getElement("rmi").appendChild(element);
+    }
+     public void removeBindedObject(String name)
+    {
+        NodeList nodeList = doc.getElementsByTagName("bindedobject");
+        Element element;
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            element = (Element) nodeList.item(i);
+            if (element.getAttribute("name").equals(name))
+            {
+                getElement("rmi").removeChild(element);
+            }
         }
     }
 }
