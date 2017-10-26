d599deb52b2ac4fccfdd83cb10ae65106e364d4f
Airat08 <myhytdinov@yandex.ru>
2017-10-26 18:56:36 +0400

    gitignore

---
+++ src/PO52/Myhytdinov/wdad/learn/rmi/Client.java
@@ -0,0 +1,7 @@
+package PO52.Myhytdinov.wdad.learn.rmi;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/PO52/Myhytdinov/wdad/learn/rmi/Server.java
@@ -0,0 +1,7 @@
+package PO52.Myhytdinov.wdad.learn.rmi;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
