4185357e936dffe0233b1e1a6f2216a7946550ba
Airat08 <myhytdinov@yandex.ru>
2017-10-26 19:34:47 +0400

    Start second the task

--- src/PO52/Myhytdinov/wdad/learn/rmi/Server.java
+++ src/PO52/Myhytdinov/wdad/learn/rmi/Server.java
@@ -1,7 +1,16 @@
 package PO52.Myhytdinov.wdad.learn.rmi;
 
-public class Server {
-    public static void main(String[] args) {
+import PO52.Myhytdinov.wdad.data.managers.PreferencesManager;
+import PO52.Myhytdinov.wdad.utils.PreferencesConstantManager;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        PreferencesManager prefManager = PreferencesManager.getInstance();
+        Context context = new InitialContext();
+        context.bind("rmi://"+prefManager.getProperty(PreferencesConstantManager.REGISTRYADDRESS)+":"+
+        prefManager.getProperty(PreferencesConstantManager.REGISTRYPORT+"Example"),null);
     }
 }
