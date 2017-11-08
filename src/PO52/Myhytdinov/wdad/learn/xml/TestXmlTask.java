a193332a654935dbcde50ea07a4b177fbbca03b3
Airat08 <myhytdinov@yandex.ru>
2017-11-08 20:17:14 +0400

    For merge

--- src/PO52/Myhytdinov/wdad/learn/xml/TestXmlTask.java
+++ src/PO52/Myhytdinov/wdad/learn/xml/TestXmlTask.java
@@ -8,8 +8,8 @@ import java.time.LocalDateTime;
 public class TestXmlTask {
     public static void main(String[] args) throws Exception {
         XmlTask xmlDoc=new XmlTask("Test.xml");
-       // XmlTask xmlDoc=new XmlTask("Test.xml");
-       // xmlDoc.loadReaders();
+        // XmlTask xmlDoc=new XmlTask("Test.xml");
+        // xmlDoc.loadReaders();
         //System.out.println(xmlDoc.getReaders()[1].getData());
 
     }
